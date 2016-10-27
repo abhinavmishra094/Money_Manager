013e85225395fa7a3dc9baf54101a84b03ddceb5
abhinavmishra094 <abhinavmishra094@gmail.com>
2016-10-27 14:49:37 +0530

    pie an line chart added

--- app/src/main/java/com/example/abhin/money_manager/daily_graph.java
+++ app/src/main/java/com/example/abhin/money_manager/daily_graph.java
@@ -1,15 +1,25 @@
 package com.example.abhin.money_manager;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.PointsGraphSeries;
+
+import java.util.ArrayList;
 
 
 /**
@@ -28,21 +38,63 @@ public class daily_graph extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_daily_graph, container, false);
-        GraphView graphView = (GraphView) view.findViewById(R.id.graph);
-        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 0),
-                new DataPoint(1, 2),
-                new DataPoint(2, 3),
-                new DataPoint(3, 4),
-                new DataPoint(4, 6),
-                new DataPoint(5, 7),
-        });
-
-        graphView.addSeries(series);
+        LineChart lineChart = (LineChart) view.findViewById(R.id.line);
+        LineData lineData = new LineData(getXAxisValues(), getDataSet());
+        lineChart.setData(lineData);
+        lineChart.setDescription("Transaction in Current Day");
+        lineChart.animateXY(2000, 2000);
+        lineChart.invalidate();
 
 
 
         return  view;
     }
+    private ArrayList<LineDataSet> getDataSet() {
+        ArrayList<LineDataSet> dataSets = null;
 
+        ArrayList<Entry> valueSet1 = new ArrayList<>();
+        Entry v1e1 = new Entry(0.00f, 0);
+        valueSet1.add(v1e1);
+        Entry v1e2 = new Entry(40.000f, 1);
+        valueSet1.add(v1e2);
+        Entry v1e3 = new Entry(60.000f, 2);
+        valueSet1.add(v1e3);
+        Entry v1e4 = new Entry(30.000f, 3);
+        valueSet1.add(v1e4);
+        Entry v1e5 = new Entry(90.000f, 4);
+        valueSet1.add(v1e5);
+        Entry v1e6 = new Entry(100.000f, 5);
+        valueSet1.add(v1e6);
+
+
+
+        LineDataSet lineDataSet1 = new LineDataSet(valueSet1, "Money Spend per month");
+        lineDataSet1.setColor(Color.rgb(0, 0, 155));
+        /*LineDataSet lineDataSet2 = new LineDataSet(valueSet2, "Brand 2");
+        lineDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+*/
+        dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet1);
+        //dataSets.add(lineDataSet2);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("6 Am");
+        xAxis.add("10 Am");
+        xAxis.add("2 Pm");
+        xAxis.add("6 pm");
+        xAxis.add("10 pm");
+        xAxis.add("12 pm");
+        /*xAxis.add("JUN");
+        xAxis.add("JULY");
+        xAxis.add("AUGUST");
+        xAxis.add("SEPTEMBER");
+        xAxis.add("OCTOBER");
+        xAxis.add("NOVEMBER");
+        xAxis.add("DECEMBER");*/
+        return xAxis;
+    }
 }
+
--- app/src/main/java/com/example/abhin/money_manager/pie_fragment.java
+++ app/src/main/java/com/example/abhin/money_manager/pie_fragment.java
@@ -1,6 +1,9 @@
 package com.example.abhin.money_manager;
 
 
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,13 +11,32 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.Highlight;
+import com.github.mikephil.charting.utils.PercentFormatter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 
 public class pie_fragment extends Fragment {
 
     PieChart pieChart;
+    int yData[] ={60,15,25,50};
+    ArrayList<Integer> y;
+    String xData[] ={"Food","Shopping","Fuel","Misc"};
+
+    MyDatabase myDatabase;
+    Cursor cursor;
+    HashMap<String,Integer> set;
 
     public pie_fragment() {
 
@@ -27,10 +49,144 @@ public class pie_fragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_pie_fragment, container, false);
         pieChart = (PieChart) view.findViewById(R.id.g_pie);
-        PieData pieData = new PieData();
+        pieChart.setDescription("");
+        pieChart.setDrawSliceText(false);
 
 
+       /* y = new ArrayList<>();
+        set = new HashMap<>() ;
+       // new aysc().execute();
+
+        myDatabase = new MyDatabase(getActivity());
+        myDatabase.open();
+        cursor = myDatabase.getTransactions();
+        while (cursor.moveToNext())
+        {
+            set.put(cursor.getString(3),cursor.getInt(1));
+        }
+        for (Map.Entry<String, Integer> e : set.entrySet()) {
+            Object key = e.getKey();
+            if(e.getKey().toString()=="Food")
+            {
+                xData[0]= "Food";
+            }
+            else if(e.getKey().toString()=="Shopping")
+            {
+                xData[1]= "Shopping";
+            }
+            else if(e.getKey().toString()=="Fuel")
+            {
+                xData[2]= "Fuel";
+            }
+            else if(e.getKey().toString()=="Misc")
+            {
+                xData[3]= "Misc";
+            }
+            Object value = e.getValue();
+          //  xData[i++]= key.toString();
+            switch (key.toString())
+            {
+                case "Food":
+                    yData[0]+=Integer.parseInt(value.toString());
+                    break;
+                case "Shopping":
+                    yData[1]+=Integer.parseInt(value.toString());
+                    break;
+                case "Fuel":
+                    yData[2]+=Integer.parseInt(value.toString());
+                    break;
+                case "Misc":
+                    yData[3]+=Integer.parseInt(value.toString());
+                    break;
+            }
+        }*/
+
+       // PieData pieData = new PieData();
+        //pieChart.animateY(5000);
+        pieChart.setBackgroundColor(Color.parseColor("#55656C"));
+        pieChart.setUsePercentValues(true);
+        //pieChart.setDescription("Mney Spend On");
+
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+                if (e == null)
+                    return;
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+        addData();
+        Legend legend = pieChart.getLegend();
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_RIGHT);
+        legend.setXEntrySpace(7);
+        legend.setYEntrySpace(5);
         return view;
     }
 
+    private void addData() {
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < yData.length; i++)
+            yVals1.add(new Entry(yData[i], i));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < xData.length; i++)
+            xVals.add(xData[i]);
+
+        // create pie data set
+        PieDataSet dataSet = new PieDataSet(yVals1, "");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(2);
+
+        // add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        // instantiate pie data object now
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        pieChart.setData(data);
+
+        // undo all highlights
+        pieChart.highlightValues(null);
+
+        // update pie chart
+        pieChart.invalidate();
+    }
+/*
+    public class aysc extends AsyncTask<Void,Void,Void>
+    {
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+           myDatabase.open();
+            return null;
+        }
+    }*/
 }
